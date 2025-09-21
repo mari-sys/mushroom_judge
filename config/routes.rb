92433a95cc00673d6dfadaf90b6a6ce813e1fcc1
mariando <mariando@mariandonoMacBook-Air.local>
2025-09-21 11:42:31 +0900

    Lintチェック修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root 'quiz#index'
+  root "quiz#index"
 
-  get 'quiz', to: 'quiz#index', as: :quiz
-  get 'quiz/question/:number', to: 'quiz#show', as: :quiz_question
-  post 'quiz/answer', to: 'quiz#answer', as: :quiz_answer
-  get 'quiz/result', to: 'quiz#result', as: :quiz_result
-
+  get "quiz", to: "quiz#index", as: :quiz
+  get "quiz/question/:number", to: "quiz#show", as: :quiz_question
+  post "quiz/answer", to: "quiz#answer", as: :quiz_answer
+  get "quiz/result", to: "quiz#result", as: :quiz_result
 end
